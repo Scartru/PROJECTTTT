65bbc9cb5913cb2f923a284e4f079566d750cfb5
Scartru <scarlettlopztru@gmail.com>
2025-12-12 16:42:33 -0500

    Update Definition.java

--- Definition.java
+++ Definition.java
@@ -4,6 +4,7 @@ public class Definition {
     protected String description;
 
     /**
+     * Definition constructor that assigns a name and description
      * @param name
      * @param description
      */
@@ -12,22 +13,32 @@ public class Definition {
         this.description = description;
     }
 
+    /**
+     * @return name assigned
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @param name value that is set to name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * @return description assigned
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * @param description value that is set to description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
-
-
     
 }
